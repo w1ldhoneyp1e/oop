fc0de61d1b234564aad431fffba5b514e43fc3da
w1ldhoneyp1e <k_yashmetov@mail.ry>
2025-02-27 11:04:12 +0300

    Radix поправил тесты

--- LW1/LW1.1/Radix/test.bat
+++ LW1/LW1.1/Radix/test.bat
@@ -4,11 +4,16 @@ set PROGRAM="%~1"
 
 call :RunTest 1 "16 10 1F" "test1.txt" "From 16 to 10"
 call :RunTest 2 "10 2 -42" "test2.txt" "Negative number"
-call :RunTest 3 "10 16 2147483647" "test3.txt" "Max int number"
-call :RunTest 4 "37 10 42" "test4.txt" "Invalid notation" true
-call :RunTest 5 "5 37 42" "test5.txt" "Invalid notation" true
-call :RunTest 6 "10 16 2147483648" "test6.txt" "Overflow" true
+call :RunTest 3 "10 16 2147483647" "test3.txt" "MAX_INT"
+call :RunTest 4 "37 10 42" "test4.txt" "Incorrect source" true
+call :RunTest 5 "5 37 42" "test5.txt" "Incorrect destination" true
+call :RunTest 6 "10 16 2147483648" "test6.txt" "Overflow positiive" true
 call :RunTest 7 "10 16 j0" "test7.txt" "Incorrect input" true
+call :RunTest 8 "-10 16 20" "test8.txt" "Negative source" true
+call :RunTest 9 "10 16 -2147483649" "test9.txt" "Overflow negative" true
+call :RunTest 10 "10 16 -2147483648" "test10.txt" "MIN_INT"
+call :RunTest 11 "36 16 G2" "test11.txt" "Boundary values"
+call :RunTest 12 "2 10 110011" "test12.txt" "Boundary values"
 
 echo All tests passed
 exit /B 0
