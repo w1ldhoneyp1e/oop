780fd2b5f8019b5559fac61e97d3cc8206924bcf
kirill.yashmetov <k_yashmetov@mail.ru>
2025-03-23 22:37:38 +0300

    catch2 init

---
+++ catch2/catch2.cpp
@@ -0,0 +1,9 @@
+﻿#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include "../LW2/LW2.1/tests.cpp"
+#include "../LW2/LW2.1/LW2.1_functions.cpp"
+
+int main(int argc, char* argv[])
+{
+	return Catch::Session().run(argc, argv);
+}
