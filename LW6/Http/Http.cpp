e548cbfbca997dfe9756b488d8f723db747c2164
kirill.yashmetov <k_yashmetov@mail.ru>
2025-05-25 22:09:59 +0300

    Http написал приложение использующее класс HttpUrl

--- LW6/Http/Http.cpp
+++ LW6/Http/Http.cpp
@@ -1,7 +1,26 @@
 ﻿#include <iostream>
+#include <string>
+#include "HttpUrl.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::string line;
+    while (std::getline(std::cin, line))
+    {
+        try
+        {
+            HttpUrl url(line);
+            std::cout << "URL: " << url.GetURL() << std::endl;
+            std::cout << "  Protocol: " << (url.GetProtocol() == Protocol::HTTP ? "HTTP" : "HTTPS") << std::endl;
+            std::cout << "  Domain:   " << url.GetDomain() << std::endl;
+            std::cout << "  Port:     " << url.GetPort() << std::endl;
+            std::cout << "  Document: " << url.GetDocument() << std::endl;
+        }
+        catch (const UrlParsingError& ex)
+        {
+            std::cout << "Error: " << ex.what() << std::endl;
+        }
+    }
+    return 0;
 }
 
